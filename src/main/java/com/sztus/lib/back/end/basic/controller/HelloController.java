3d2869c476a093086dc4c02572bad2e2c1b138c3
70Austin <austinwang@sztus.com>
2024-04-25 12:04:52 +0800

    test

--- src/main/java/com/sztus/lib/back/end/basic/controller/HelloController.java
+++ src/main/java/com/sztus/lib/back/end/basic/controller/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello2")
     public Result<String> hello2() {
-        return Result.ok("hello2");
+        return Result.ok("hello2hello");
     }
 }
